d43f19d94bfee495c39d7fe62a816ac376a26fd5
outlander258 <106289052+outlander258@users.noreply.github.com>
2022-06-01 00:50:50 -0400

    Update archivo para github.py

--- archivo para github.py
+++ archivo para github.py
@@ -3,3 +3,5 @@ print("jean pierre puro que pasa raja en el metro")
 danilo = int(input("ingrese cuantos litros de licor le va a donar al profe hernan : "))
 if danilo >5:
   print("lo que sea")
+else:
+    print("danilo no pasa el ramo de algoritmos")
