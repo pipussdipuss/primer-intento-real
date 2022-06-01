1c36c1fbda7f6fa12d08a471be15d485a3f24c3d
pipussdipuss <106289042+pipussdipuss@users.noreply.github.com>
2022-06-01 00:48:15 -0400

    Update archivo para github.py

--- archivo para github.py
+++ archivo para github.py
@@ -1,3 +1,3 @@
 print("hola mundo")
 print("jean pierre puro que pasa raja en el metro")
-danilo = int(input("ingrese cuantos litros de licor le va a donar al profe hernan :"))
+danilo = int(input("ingrese cuantos litros de licor le va a donar al profe hernan : "))
