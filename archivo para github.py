8a0ab70414e0cb73ae8f72d77eb92d42e5445fe4
pipussdipuss <106289042+pipussdipuss@users.noreply.github.com>
2022-06-01 00:49:42 -0400

    Update archivo para github.py

--- archivo para github.py
+++ archivo para github.py
@@ -1,3 +1,5 @@
 print("hola mundo")
 print("jean pierre puro que pasa raja en el metro")
 danilo = int(input("ingrese cuantos litros de licor le va a donar al profe hernan : "))
+if danilo >5:
+  print("lo que sea")
